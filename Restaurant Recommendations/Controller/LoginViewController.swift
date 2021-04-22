dbb6c2659b05ea033ffebaa8134e121fb1b847c2
Shi Tao Luo <sluo5991@gmail.com>
2021-04-22 13:22:47 -0400

    created login textfields, and call backend for login

--- Restaurant Recommendations/Controller/LoginViewController.swift
+++ Restaurant Recommendations/Controller/LoginViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    //log user in once entered email, password and click login button
+    @IBAction func onLogin(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        FirebaseHelper().signInUser(userEmail: email, userPassword: password)
+    }
 }
 
