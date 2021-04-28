9155ed155b4d3f4fc767bc3a8396acde54b5d7a5
Shi Tao Luo <sluo5991@gmail.com>
2021-04-28 14:05:31 -0400

    implemented sign up

--- Restaurant Recommendations/Controller/LoginViewController.swift
+++ Restaurant Recommendations/Controller/LoginViewController.swift
@@ -9,27 +9,66 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let firebase = FirebaseHelper();
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var loginButton: UIButton!
-    
+    @IBOutlet weak var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firebase.delegate = self
     }
 
     
-    //log user in once entered email, password and click login button
-    @IBAction func onLogin(_ sender: Any) {
+    @IBAction func onSignUp(_ sender: Any) {
         let email = emailTextField.text!
         let password = passwordTextField.text!
+        firebase.signUpUser(userEmail: email, userPassword: password)
         
-        FirebaseHelper().signInUser(userEmail: email, userPassword: password)
     }
     
     
+    @IBAction func onLogin(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        firebase.signInUser(userEmail: email, userPassword: password)
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
 }
+
+extension LoginViewController: firebaseProtocols {
+    func signUpSuccessful() {
+        //take user to the home screen
+        //set user default to true
+    }
+
+    func signInSuccessful() {
+        
+    }
+
+    func signedOutSuccessful() {
+        
+    }
+
+    func error(error: Error) {
+        print("Error Signing Up: \(error.localizedDescription)")
+    }
+
+    func friendAdded() {}
+
+    func restaurantAdded() {}
+
+    func profilePictureUploaded() {}
+}
+
 
