65de15e69f964876c37541c6dba1ab6f8626ca62
Shi Tao Luo <sluo5991@gmail.com>
2021-04-28 14:52:48 -0400

    Fixed login and signup

--- Restaurant Recommendations/Controller/LoginViewController.swift
+++ Restaurant Recommendations/Controller/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
     
@@ -31,29 +32,40 @@ class LoginViewController: UIViewController {
         
     }
     
-    
     @IBAction func onLogin(_ sender: Any) {
         let email = emailTextField.text!
         let password = passwordTextField.text!
         firebase.signInUser(userEmail: email, userPassword: password)
     }
     
-    
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if UserDefaults.standard.bool(forKey: "loginSuccess")
+        {
+            performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
 }
 
 extension LoginViewController: firebaseProtocols {
     func signUpSuccessful() {
         //take user to the home screen
         //set user default to true
+        UserDefaults.standard.set(true, forKey: "loginSuccess")
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
     }
 
     func signInSuccessful() {
-        
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
     }
 
     func signedOutSuccessful() {
