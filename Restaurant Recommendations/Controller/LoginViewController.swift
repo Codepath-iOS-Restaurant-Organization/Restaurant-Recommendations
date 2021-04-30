3880defed1b55e0f299197e618ccb6e745e20b3c
Feizza Fazilatun <feizza01@gmail.com>
2021-04-29 23:01:24 -0400

    Added logout and addFriend bar buttons

--- Restaurant Recommendations/Controller/LoginViewController.swift
+++ Restaurant Recommendations/Controller/LoginViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    let firebase = FirebaseHelper()
+    @IBAction func onLogin(_ sender: Any) {
+        firebase.signInUser(userEmail: "colors@gmail.com", userPassword: "colors")
+        self.performSegue(withIdentifier: "loginToHome", sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        firebase.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension LoginViewController: firebaseProtocols{
+    func signUpSuccessful() {
+        print("Signed up")
+    }
+    
+    func signInSuccessful() {
+        print("signed in")
+    }
+    
+    func signedOutSuccessful() {
+        
+    }
+    
+    func error(error: Error) {
+        print("unsuccess \(error.localizedDescription)")
+    }
+    
+    func friendAdded() {
+        
+    }
+    
+    func restaurantAdded() {
+        
+    }
+    
+    func profilePictureUploaded() {
+        
+    }
+    
+    
+}
