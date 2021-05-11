032e29970aa0af8b3006d16afbefacc16451e83d
Shi Tao Luo <sluo5991@gmail.com>
2021-05-11 19:41:40 -0400

    started recommendation tableview

--- Restaurant Recommendations/Controller/RecommendationTableViewController.swift
+++ Restaurant Recommendations/Controller/RecommendationTableViewController.swift
@@ -6,10 +6,17 @@
 //
 
 import UIKit
+import Firebase
 
-class RecommendationTableViewController: UITableViewController {
-
+class RecommendationTableViewController: UITableViewController
+{
     var friendChosen = ""
+    var currentUser: UserInformation?
+    var friend: UserInformation?
+    
+//    let similarArray = []
+//    let differentArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,19 +24,67 @@ class RecommendationTableViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print(friendChosen)
+                
+        if let current = Auth.auth().currentUser?.email
+        {
+            currentUser?.getUserFriends(email: current)
+            friend?.getUserFriends(email: friendChosen)
+            
+            friend?.userReturned.favoriteRestaurants
+            currentUser?.userReturned.favoriteRestaurants
+        }
     }
 
     // MARK: - Table view data source
+    
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        
+//        if section == 0
+//        {
+//            return similarArray.count
+//        }
+//        if section == 1
+//        {
+//            return differentArray.count
+//        }
+        
         return 0
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! RestaurantCell
+        
+        let dummyCell = UITableViewCell()
+        
+        if (indexPath.section == 0)
+        {
+            dummyCell.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        }
+        else if (indexPath.section == 1)
+        {
+            return dummyCell
+        }
+        return dummyCell
+    }
 
+    //title section
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        if (section == 0){
+            return "Recommendations"
+        }
+        else {
+            return "Other"
+        }
+    }
+    
 
 }
