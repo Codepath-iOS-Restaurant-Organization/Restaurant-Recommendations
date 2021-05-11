498b4307d04bef55663d173e51a15329b32d678a
Richard Basdeo <basdeorichard@gmail.com>
2021-05-11 18:09:37 -0400

    Added UI elements for RestaurantDetailView. Implemented Add to favorites button. - Richard

--- Restaurant Recommendations/Controller/RestaurantDetailViewController.swift
+++ Restaurant Recommendations/Controller/RestaurantDetailViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import Firebase
 
 class RestaurantDetailViewController: UIViewController {
 
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var typeOfFoodlabel: UILabel!
+    @IBOutlet weak var dollarSignLabel: UILabel!
+    
+    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var totalAmountOfReviewsLabel: UILabel!
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    @IBOutlet weak var streetCityLabel: UILabel!
+    @IBOutlet weak var stateZipCodeLabel: UILabel!
+    
+    
+    
+    var chosenRestaurant: Restaurant?
+    let fire = FirebaseHelper()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        fire.delegate = self
     }
     
+    @IBAction func addToFavoritesPressed(_ sender: UIButton) {
+    
+        if let id = chosenRestaurant?.restaurantID {
+            fire.addFavoriteRestaurant(theID: id)
+        }
+    }
+    
+}
 
+extension RestaurantDetailViewController: firebaseProtocols {
+    
+    func restaurantAdded() {
+        
+        navigationController?.popToRootViewController(animated: true)
+        
+        
+        
+    }
+    func error(error: Error) {
+        
+        print(error.localizedDescription)
+        
+    }
+    
+    func signUpSuccessful() {}
+    func signInSuccessful() {}
+    func signedOutSuccessful() {}
+    func friendAdded() {}
+    func profilePictureUploaded() {}
+    
 }
